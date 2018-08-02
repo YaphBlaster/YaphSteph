11a3349aac90d61901abddbadb183f2f884e235f
Yaphet Abraha <yaphet@Yaphets-MacBook-Pro.local>
2018-08-02 14:18:14 -0500

    added react spring and changed back to top button

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import Albums from "./components/Albums";
 import Data from "./data/Data";
 import { addBackToTop } from "vanilla-back-to-top";
 import ImagesLoaded from "react-images-loaded";
+import { Spring, animated } from "react-spring";
 
 class App extends Component {
   state = {
     loaded: false
   };
   render() {
-    addBackToTop();
+    addBackToTop({
+      backgroundColor: "#ffcd3a"
+    });
     return (
-      <div className={`App ${this.state.loaded ? "" : "hidden"}`}>
-        <ImagesLoaded done={() => this.setState({ loaded: true })}>
-          <Intro title="Yaphet and Stephanie" />
-          <Albums albums={Data.photoLinks} />
-        </ImagesLoaded>
-      </div>
+      <Spring native from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {styles => (
+          <animated.div style={styles}>
+            <div className={`App ${this.state.loaded ? "" : "hidden"}`}>
+              <ImagesLoaded done={() => this.setState({ loaded: true })}>
+                <Intro title="Yaphet and Stephanie" />
+                <Albums albums={Data.photoLinks} />
+              </ImagesLoaded>
+            </div>
+          </animated.div>
+        )}
+      </Spring>
     );
   }
 }
