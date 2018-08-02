bf2a22c6551972ca295e6899d21ff110057d64e5
Yaphet Abraha <yaphet@Yaphets-MacBook-Pro.local>
2018-08-02 16:19:56 -0500

    spring removal and back to top button fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Albums from "./components/Albums";
 import Data from "./data/Data";
 import { addBackToTop } from "vanilla-back-to-top";
 import ImagesLoaded from "react-images-loaded";
-import { Spring, animated } from "react-spring";
 
 class App extends Component {
   state = {
@@ -15,18 +14,12 @@ class App extends Component {
       backgroundColor: "#ffcd3a"
     });
     return (
-      <Spring native from={{ opacity: 0 }} to={{ opacity: 1 }}>
-        {styles => (
-          <animated.div style={styles}>
-            <div className={`App ${this.state.loaded ? "" : "hidden"}`}>
-              <ImagesLoaded done={() => this.setState({ loaded: true })}>
-                <Intro title="Yaphet and Stephanie" />
-                <Albums albums={Data.photoLinks} />
-              </ImagesLoaded>
-            </div>
-          </animated.div>
-        )}
-      </Spring>
+      <div className={`App ${this.state.loaded ? "" : "hidden"}`}>
+        <ImagesLoaded done={() => this.setState({ loaded: true })}>
+          <Intro title="Yaphet and Stephanie" />
+          <Albums albums={Data.photoLinks} />
+        </ImagesLoaded>
+      </div>
     );
   }
 }
